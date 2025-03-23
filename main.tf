d90dd489cf76fd78475b2cd2911d33a55d9342e9
Eyungsi <yuyunericyungsi@gmail.com>
2025-03-22 21:50:26 -0400

    second comit

---
+++ main.tf
@@ -0,0 +1,13 @@
+# Create a new GitLab Lightsail Instance
+resource "aws_lightsail_instance" "terraform-server" {
+  name              = "Dev-server"
+  availability_zone = "us-east-1a"
+  blueprint_id      = "amazon_linux_2"
+  bundle_id         = "nano_3_0"
+  key_pair_name     = "Week2key"
+  tags = {
+    env  = "dev"
+    Team = "Cloud"
+  }
+}
+
